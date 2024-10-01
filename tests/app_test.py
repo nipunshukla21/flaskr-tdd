4c326a9732d581d6d9315c0f55a7f9b03988c77d
nipunshukla21 <nipunshukla002@gmail.com>
2024-10-01 12:59:48 -0400

    Added search functionality

--- tests/app_test.py
+++ tests/app_test.py
@@ -80,3 +80,14 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_search(client):
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.post(
+        "/add",
+        data=dict(title="Search test", text="<strong>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
+    response = client.get("/search/?query=test")
+    assert response.status_code == 200
+    assert b"Search test" in response.data
